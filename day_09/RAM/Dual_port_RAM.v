eb0ff474a3bb32e75f59986722f49ac982ccefc1
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-01-03 23:31:54 +0530

    Dual port RAM

--- day_09/RAM/Dual_port_RAM.v
+++ day_09/RAM/Dual_port_RAM.v
@@ -5,8 +5,7 @@ module dual_port_ram #(parameter DEPTH = 16, WIDTH = 16)(
     input w_en_a, w_en_b,
     input [$clog2(DEPTH)-1:0] addr_a, addr_b,
     input [WIDTH-1:0] data_in_a, data_in_b,
-    output reg [WIDTH-1:0] data_out_a, data_out_b
-);
+    output reg [WIDTH-1:0] data_out_a, data_out_b);
 
 reg [WIDTH-1:0] mem [0:DEPTH-1];
 
