cef2e9fbf71b1305612412a359b41731489f0174
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-01-16 22:44:15 +0530

    Single port RAM

--- day_09/RAM/Single port RAM.v
+++ day_09/RAM/Single port RAM.v
@@ -1,18 +1,24 @@
 //Single port RAM
-module single_port_RAM #(parameter DEPTH = 8, WIDTH = 8)(
-  input clk, w_en, 
-  input [$clog(DEPTH)-1:0] addr,
-  input [WIDTH-1:0] data_in,
-  output reg [WIDTH-1:0] data_out);
+module single_port_RAM #(
+    parameter DEPTH = 8, 
+    parameter WIDTH = 8
+)(
+    input clk, 
+    input w_en, 
+    input [$clog2(DEPTH)-1:0] addr, 
+    input [WIDTH-1:0] data_in,
+    output reg [WIDTH-1:0] data_out
+);
 
-  reg [WIDTH-1:0] mem [0:DEPTH-1];
+    // Memory array
+    reg [WIDTH-1:0] mem [0:DEPTH-1];
   
-  always@(posedge clk)begin
-    if(w_en)begin
-      mem[addr] <= data_in;
-    else
-      data_out <= mem[addr];
-  end
+    always @(posedge clk) begin
+        if (w_en) begin
+            mem[addr] <= data_in;
+        end 
+        data_out <= mem[addr]; 
+    end
 
 endmodule
 
