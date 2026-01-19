08fca30cb9fd1c9c5791c885d973fec3670fdc12
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-01-19 22:39:27 +0530

    Dual port RAM

--- day_09/RAM/Dual_port_RAM.v
+++ day_09/RAM/Dual_port_RAM.v
@@ -1,35 +1,26 @@
+// Dual port RAM
 module dual_port_ram #(parameter DEPTH = 16, WIDTH = 16)(
-    input clk,
-    input rst,                      
+    input clk, rst,
     input w_en_a, w_en_b,
     input [$clog2(DEPTH)-1:0] addr_a, addr_b,
     input [WIDTH-1:0] data_in_a, data_in_b,
-    output reg [WIDTH-1:0] data_out_a, data_out_b
-);
+    output reg [WIDTH-1:0] data_out_a, data_out_b);
 
-reg [WIDTH-1:0] mem [0:DEPTH-1];
+  reg [WIDTH-1:0] mem [0:DEPTH-1];
 
-always @(posedge clk) begin
-    if (rst) begin
-        data_out_a <= 0;
-        data_out_b <= 0;
-    end else begin
-        // PORT A Logic
-        if(w_en_a)
-            mem[addr_a] <= data_in_a;
-        else
-            data_out_a <= mem[addr_a];
+  always @(posedge clk) begin
+    // PORT A
+    if(w_en_a)
+        mem[addr_a] <= data_in_a;
+    data_out_a <= mem[addr_a];
 
-        // PORT B Logic
-        if(w_en_b)
-            mem[addr_b] <= data_in_b;
-        else
-            data_out_b <= mem[addr_b];
-    end
-end
+    // PORT B
+    if(w_en_b)
+        mem[addr_b] <= data_in_b;
+    data_out_b <= mem[addr_b]; 
+  end
 
 endmodule
-
 // testbench
 
 `timescale 1ns/1ps
