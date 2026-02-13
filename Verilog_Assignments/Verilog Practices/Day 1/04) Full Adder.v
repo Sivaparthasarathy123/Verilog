6307fe4e625a0dd73643658338690edcef1b7346
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-02-13 23:29:03 +0530

    Add Full Adder module and testbench in Verilog

---
+++ Verilog_Assignments/Verilog Practices/Day 1/04) Full Adder.v
@@ -0,0 +1,34 @@
+// Full Adder
+module full_adder(
+  input a,
+  input b,
+  input c,
+  output reg sum,
+  output reg carry
+);
+  
+  always@(*)begin
+    sum   = a ^ b ^ c;
+    carry = (a & b)||(b & c)||(a & c);
+  end
+endmodule
+
+module full_adder_tb;
+  reg a, b, c;
+  wire sum, carry;
+  
+  full_adder dut (a, b, c, sum, carry);
+  
+  integer i;
+  initial begin
+    $monitor(" A = %b | B = %b | C = %b | SUM = %b | CARRY = %b ", a, b, c, sum, carry);
+    
+    for(i = 0; i < 8; i++) begin
+      {a, b, c} = $urandom;  
+      #10;
+    end
+
+     $finish;
+    end
+
+endmodule
