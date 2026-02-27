5d2cc82c262c643475304c107b99a7a3c2102ccf
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-02-28 00:00:45 +0530

    Zero detector

---
+++ Verilog_Assignments/Verilog Practices/Day 6/05) Zero detector.v
@@ -0,0 +1,26 @@
+// zero detector
+module zero_detector(
+  input  [15:0] A,
+  output Z
+);
+  assign Z = (A == 16'd0);
+endmodule
+
+// Testbench
+module zero_detector_tb;
+  reg [15:0] A;
+  wire Z;
+
+  zero_detector uut (
+      .A(A),
+      .Z(Z)
+  );
+
+  initial begin
+    $monitor("A = %0d | Z = %0d");
+      A = 16'd0;  #10;
+      A = 16'd5;  #10;
+      A = 16'd65535; #10;
+      $finish;
+  end
+endmodule
