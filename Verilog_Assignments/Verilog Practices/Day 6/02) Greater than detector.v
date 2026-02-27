d539579566bc20c988c6773e9e5d35014eea2ebe
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-02-27 23:44:17 +0530

    Create 02) Greater than detector.v

---
+++ Verilog_Assignments/Verilog Practices/Day 6/02) Greater than detector.v
@@ -0,0 +1,28 @@
+// Greater than detector
+module greater_than_detector(
+  input  [15:0] A,
+  input  [15:0] B,
+  output GT
+);
+  assign GT = (A > B);
+endmodule
+
+// Testbench
+module greater_than_detector_tb;
+  reg [15:0] A, B;
+  wire GT;
+
+  greater_than_detector uut (
+      .A(A),
+      .B(B),
+      .GT(GT)
+  );
+
+  initial begin
+    $monitor("Time = %0t | A = %0d | B = %0d | GT = %0d", $time, A, B, GT);
+      A = 16'd10; B = 16'd5;  #10;
+      A = 16'd7;  B = 16'd7;  #10;
+      A = 16'd2;  B = 16'd8;  #10;
+      $finish;
+  end
+endmodule
