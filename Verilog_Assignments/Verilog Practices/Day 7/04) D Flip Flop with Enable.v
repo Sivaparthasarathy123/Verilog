eb7fca49ac1b9a1a7fadb69a8d6564204e6eef51
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-03-04 00:03:42 +0530

    D Flip Flop with Enable

---
+++ Verilog_Assignments/Verilog Practices/Day 7/04) D Flip Flop with Enable.v
@@ -0,0 +1,37 @@
+// D Flip Flop Enable
+module d_ff_en (
+  input  wire clk,
+  input  wire en,
+  input  wire d,
+  output reg  q
+);
+    
+  always @(posedge clk) begin
+    if (en)
+      q <= d;
+    end
+  
+endmodule
+
+// Testbench
+module d_ff_en_tb;
+
+  reg clk, en, d;
+  wire q;
+
+  d_ff_en dut (.clk(clk), .en(en), .d(d), .q(q));
+
+    initial clk = 0;  
+    always #5 clk = ~clk;
+
+    initial begin
+      $monitor("Time = %0t clk = %b d = %b q = %b en = %b", $time, clk, d, q, en);
+
+        en = 0;#10;
+        en = 1;#10;
+        d = 0; #15;
+        d = 1; #10;
+        d = 0; #10;
+        $finish;
+    end
+endmodule
