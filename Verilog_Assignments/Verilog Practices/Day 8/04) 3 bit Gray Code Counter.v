5e8fdf32ceb0cc68053919569b875f15cc1d85ae
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-03-06 00:16:13 +0530

    3 bit Gray Code Counter

--- Verilog_Assignments/Verilog Practices/Day 8/04) 3 bit Gray Code Counter.v
+++ Verilog_Assignments/Verilog Practices/Day 8/04) 3 bit Gray Code Counter.v
@@ -1,46 +1,43 @@
 // 3-bit Gray Code Counter
 module gray_counter_3bit (
-    input clk,
-    input rst,
-    output reg [2:0] gray
+  input clk,
+  input rst,
+  output reg [2:0] gray
 );
 
-    reg [2:0] binary;
+  reg [2:0] binary;
 
-    // Binary counter
-    always @(posedge clk or posedge rst) begin
-        if (rst)
-            binary <= 3'b000;
-        else
-            binary <= binary + 1;
+  always @(posedge clk or posedge rst) begin
+    if (rst) begin
+      binary <= 3'b000;
+      gray   <= 3'b000;
     end
-
-    // Binary to Gray conversion
-    always @(*) begin
-        gray = binary ^ (binary >> 1);
+    else begin
+      binary <= binary + 1;
+      gray   <= (binary + 1) ^ ((binary + 1) >> 1);
     end
+  end
 
 endmodule
 
 // Testbench
 module gray_counter_tb;
+  reg clk;
+  reg rst;
+  wire [2:0] gray;
 
-    reg clk;
-    reg rst;
-    wire [2:0] gray;
+  gray_counter_3bit dut (clk, rst, gray);
 
-  gray_counter_3bit dut (clk,rst,dray);
+  initial clk = 0;
+  always #5 clk = ~clk;
 
-    initial clk = 0;
-    always #5 clk = ~clk;
+  initial begin
+    $monitor("Time = %0t clk = %b rst = %b gray = %b",$time,clk,rst,gray);
 
-    initial begin
-      $monitor("Time = %0t clk = %b rst = %b gray = %b",$time,clk,rst,gray);
+    rst = 1;
+    #12 rst = 0;
 
-        rst = 1;
-        #15 rst = 0;
-
-        #100 $finish;
-    end
+    #100 $finish;
+  end
 
 endmodule
