b8113c0712550ebc4f40d4e91dcc83752d6078b6
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-03-08 22:50:23 +0530

    Frequency Divider (by 5)

---
+++ Verilog_Assignments/Verilog Practices/Day 9/08) Frequency Divider (by 5).v
@@ -0,0 +1,47 @@
+// Frequency Divider (by 5)
+module freq_div5(
+  input clk,
+  input rst,
+  output reg out
+);
+
+  reg [2:0] count;
+
+  always @(posedge clk or posedge rst) begin
+    if(rst) begin
+      count <= 0;
+      out <= 0;
+    end
+    else begin
+      if(count == 4) begin
+        count <= 0;
+        out <= ~out;
+      end
+      else
+        count <= count + 1;
+    end
+  end
+
+endmodule
+
+// Testbench
+module freq_div5_tb;
+  reg clk;
+  reg rst;
+  wire out;
+
+  freq_div5 dut (clk,rst,out);
+
+  initial clk = 0;
+  always #5 clk = ~clk; 
+
+  initial begin
+    $monitor("Time = %0t clk = %b rst = %b out = %b", $time, clk, rst, out);
+
+    rst = 1;
+    #12 rst = 0;
+
+    #200 $finish;
+  end
+
+endmodule
